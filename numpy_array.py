db41f4d5777e27cc9a107b3869c94eadca7a4b81
imswaggyboy <75663923+imswaggyboy@users.noreply.github.com>
2022-11-04 03:10:18 +0530

    Create numpy_array.py

---
+++ numpy_array.py
@@ -0,0 +1,12 @@
+
+# just put the given input and used reverse indexing 
+def arrays(arr):
+  numpy_arr =  numpy.array(arr[::-1],dtype = float)
+  return numpy_arr
+
+# used numpy method flip()
+def arrays(arr):
+  # numpy_arr =  numpy.array(arr[::-1],dtype = float)
+  arrr = numpy.array(arr,float)
+  
+  return numpy.flip(arrr)
