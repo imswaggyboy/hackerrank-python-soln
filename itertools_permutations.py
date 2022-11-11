221cc84f9ffd9ac5ff88eaf36de41125e40a6cc9
Surya Murugan <75663923+imswaggyboy@users.noreply.github.com>
2022-11-11 15:10:11 +0530

    Delete itertools_permutations.py

--- itertools_permutations.py
+++
@@ -1,7 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-from itertools import permutations
-n ,m = input().split()
-m = int(m)
-permu = list(permutations(n,m))
-for i in sorted(permu):
-    print(''.join(i))
